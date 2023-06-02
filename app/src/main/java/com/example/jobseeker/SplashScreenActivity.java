ea63fd082f7a9bdd64b82690883a327def92c8a5
karthik <karthik@gmail.com>
2023-06-02 19:31:06 +0530

    normal changes 11

--- app/src/main/java/com/example/jobseeker/SplashScreenActivity.java
+++ app/src/main/java/com/example/jobseeker/SplashScreenActivity.java
@@ -1,46 +1,23 @@
 package com.example.jobseeker;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private  static  int SPLASH_SCREEN =3500;
-    ImageView imageView;
-    TextView textView1, textView2;
-    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splashscreen);
-
-        imageView = findViewById(R.id.imageView);
-        textView1 = findViewById(R.id.textView);
-        top = AnimationUtils.loadAnimation(this, R.anim. top);
-        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom);
-
-        imageView.setAnimation(top);
-        textView1.setAnimation(bottom);
-
+        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
             }
-        }, SPLASH_SCREEN);
-
+        },5000);
     }
 }
